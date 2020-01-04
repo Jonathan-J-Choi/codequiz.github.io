aa32a99d78d5923d9d0b5ddcd94403583c32a64d
Jonathan Choi <Jonathan.Junghoon.Choi@gmail.Com>
2020-01-04 18:41:22 -0500

    first commit

---
+++ questions.js
@@ -0,0 +1,32 @@
+var questions = [
+  {
+    title: "Javascript's actual name is ____.",
+    choices: ["Java", "HTML", "C++", "ECMA Script"],
+    answer: "ECMA Script"
+  },
+
+  {
+    title: "JavaScript MAINLY deals with the ____ of a website.",
+    choices: ["links", "appearance", "interactivity", "smell"],
+    answer: "appearance"
+  },
+
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "numbers", "alerts"],
+    answer: "alerts"
+  },
+
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+
+  {
+    title: "Variables are the English equivalent of what in Javascript?",
+    choices: ["nouns", "verbs", "predicates", "adjectives"],
+    answer: "nouns"
+  },
+
+];
